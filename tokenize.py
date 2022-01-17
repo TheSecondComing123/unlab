51f1df83f2ea61d8cdc5e836721d204f350b30ee
JiuTheProgrammer123 <jiuprogrammer@gmail.com>
2022-01-17 14:16:27 -0500

    tokenizer

--- tokenize.py
+++ tokenize.py
@@ -1,3 +1,14 @@
+import enum
+
+
+class TokenType(enum.Enum):
+    NUMBER = "number"
+    STRING = "string"
+    FLOAT = "float"
+    LIST = "list"
+    DICT = "dict"
+
+
 class Token:
     def __init__(self, name, value):
         self.name = name
