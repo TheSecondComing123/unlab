27a1e1df7825eb5c7ba730aa934c9911b001b879
JiuTheProgrammer123 <jiuprogrammer@gmail.com>
2022-01-18 12:03:01 -0500

    easier addition

--- tokenize.py
+++ tokenize.py
@@ -5,11 +5,19 @@ import re
 class TokenType(enum.Enum):
     NUMBER = "number"
     ADD = "add"
+    SUB = "sub"
+    MUL = "mul"
+    DIV = "div"
+    NUMBSEP = "numbsep"
 
 
 class TokenRe(enum.Enum):
     NUMBER_RE = r"\d"
     ADD_RE = r"\+"
+    SUB_RE = r"-"
+    MUL_RE = r"\*"
+    DIV_RE = r"/"
+    NUMBSEP_RE = r"\s"
 
 
 class Token:
@@ -28,13 +36,33 @@ def gettok(name, val, *, converter=_none):
     return Token(name.value, converter(val))
 
 
+def handle(regex, c, tokenlist, name, *, converter=_none):
+    if re.match(regex.value, c):
+        tokenlist.append(gettok(name, c, converter=converter))
+
+
 def tokenize(text):
     tokens = []
     for char in text:
-        if re.match(TokenRe.NUMBER_RE.value, char):
-            tokens.append(gettok(TokenType.NUMBER, char,
-                                 converter=int))
-        elif re.match(TokenRe.ADD_RE.value, char):
-            tokens.append(gettok(TokenType.ADD, char))
+        # if re.match(TokenRe.NUMBER_RE.value, char):
+        #     tokens.append(gettok(TokenType.NUMBER, char,
+        #                          converter=int))
+        # elif re.match(TokenRe.ADD_RE.value, char):
+        #     tokens.append(gettok(TokenType.ADD, char))
+        # elif re.match(TokenRe.SUB_RE.value, char):
+        #     tokens.append(gettok(TokenType.SUB, char))
+        # elif re.match(TokenRe.MUL_RE.value, char):
+        #     tokens.append(gettok(TokenType.MUL, char))
+        # elif re.match(TokenRe.DIV_RE.value, char):
+        #     tokens.append(gettok(TokenType.DIV, char))
+        # elif re.match(TokenRe.NUMBSEP_RE.value, char):
+        #     tokens.append(gettok(TokenType.NUMBSEP, char))
+        handle(TokenRe.NUMBER_RE, char, tokens, TokenType.NUMBER, converter=int)
+        handle(TokenRe.ADD_RE, char, tokens, TokenType.ADD)
+        handle(TokenRe.SUB_RE, char, tokens, TokenType.SUB)
+        handle(TokenRe.MUL_RE, char, tokens, TokenType.MUL)
 
     return tokens
+
+
+print(tokenize("+1 -99 *4 3"))
