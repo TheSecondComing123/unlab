dafdc4bc1a3d878412f43e693df2b61f5aeb85f1
JiuTheProgrammer123 <jiuprogrammer@gmail.com>
2022-01-20 10:42:04 -0500

    added functions.py

---
+++ functions.py
@@ -0,0 +1,17 @@
+g = "Hello, World!"
+w = "Hello World"
+l = "hello world"
+b = "0123456789"
+o = "123456789"
+a = "abcdefghijklmnopqrstuvwxyz"
+u = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+p1 = 2
+p2 = 4
+p3 = 16
+p4 = 256
+p5 = 32768
+p6 = 65536
+p7 = 2048
+p8 = 4906
+p9 = 1024
+pt = 128
