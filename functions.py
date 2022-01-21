c1ee1ef303766f46a42dd36252c858c58a50a494
JiuTheProgrammer123 <jiuprogrammer@gmail.com>
2022-01-21 10:33:12 -0500

    added stuff

--- functions.py
+++ functions.py
@@ -1,16 +1,20 @@
+from operator import add
+
 g = "Hello, World!"
 w = "Hello World"
-l = "hello world"
 b = "0123456789"
 o = "123456789"
 a = "abcdefghijklmnopqrstuvwxyz"
 u = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-p0 = 10
-p1 = 16
-p2 = 256
-p3 = 32768
-p4 = 65536
-p5 = 2048
-p6 = 4906
-p7 = 1024
-p8 = 128
+c = 10
+d = 16
+e = 256
+f = 32768
+h = 65536
+i = 2048
+j = 4906
+k = 1024
+l = 128
+n = -1
+
+functions = {"+": [2, add]}
