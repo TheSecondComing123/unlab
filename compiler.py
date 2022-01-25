196b13ba9f10b880082fa62255e92ddf699113b8
Tams-Tams <Tams.Mathe@gmail.com>
2022-01-25 20:58:28 +0100

    almost working

--- compiler.py
+++ compiler.py
@@ -1,14 +1,20 @@
+from parser import parse
+from tokenizer import tokenize
+from elements import elements
+
+
 def compile(tokens):
-    for i, sub in enumerate(tokens):
-        for j, e in enumerate(sub):
-            if isinstance(e, list):
-                val = compile(e)
-            else:
-                func = sub[0]
-                args = sub[1:]
-                val = func(args)
-            tokens[i][j] = val
+    for i, sub in enumerate(tokens.copy()):
+        if isinstance(sub[0], list):
+            val = compile(sub[0])
+        else:
+            func = sub[0]
+            args = sub[1:]
+            val = elements[func.value][1](args)
+            tokens[i] = val
 
     return tokens
 
 
+if __name__ == "__main__":
+    print(compile(parse(tokenize("+1 23"))))
