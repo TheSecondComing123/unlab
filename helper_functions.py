49cdb479c046caec2a2a9d0abefaff3ca95ad157
Tams-Tams <Tams.Mathe@gmail.com>
2022-02-07 16:23:29 +0100

    no
    well

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,1 @@
 """File for helper functions"""
-
-
-def to_python_index(lst, indexes):
-    return f"{lst}{''.join(map(lambda x:'[' + str(x) + ']', indexes))}"
--- interpreter.py
+++ interpreter.py
@@ -1,43 +1,24 @@
 from tokenizer import tokenize, Token, TokenType
 from elements import elements
 from parse import parse
-from helper_functions import to_python_index
 
 
-class Interpreter:
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.position = []
+def interprete(tokens: list[Token]) -> Token:
+    if type(tokens) is list:
+        for token in tokens:
+            if type(token) is list:
+                func = elements.get(token[0].value)[1]
+                args = map(interprete, token[1:])
+                result = func(list(args))
+            else:
+                result = tokens
+    else:
+        result = tokens
 
-    def main(self, tokens):
-        self.position.append(0)
-
-        for index, sub in enumerate(tokens):
-            self.position[-1] = index
-            if isinstance(sub, list):
-                val = self.main(sub)
-                del self.position[-1]
-
-                exec(to_python_index("self.tokens", self.position[:-1]) + f"={val[0]}")
-
-        try:
-            func = tokens[0]
-            args = tokens[1:]
-            val = elements[func.value][1](args)
-            exec(to_python_index("self.tokens", self.position[:-1]) + f"={val}")
-
-            return self.tokens
-        except (KeyError, AttributeError):
-            return self.tokens
-
-
-    @staticmethod
-    def interprete(tokens):
-        runner = Interpreter(tokens)
-        return runner.main(runner.tokens).value
+    return result
 
 
 if __name__ == "__main__":
-    print(Interpreter.interprete(parse(tokenize("+1 3"))))
-    print(Interpreter.interprete(parse(tokenize("+ +1 3 4"))))
-    print(Interpreter.interprete(parse(tokenize("+ +1 3 +4 5"))))
+    print(interprete(parse(tokenize("+1 3"))))
+    print(interprete(parse(tokenize("+ +1 3 4"))))
+    print(interprete(parse(tokenize("+ +1 3 +4 5"))))
