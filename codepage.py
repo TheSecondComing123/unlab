3c3401e4267fdb17ea19aa335df0160b20d16d81
JiuTheProgrammer123 <jiuprogrammer@gmail.com>
2022-03-16 09:49:29 -0400

    lol

--- codepage.py
+++ codepage.py
@@ -1,5 +1,8 @@
-FUNCTIONS = ["+", "-", "×", "÷", "ⁱ", "g", "¶", "Đ", "Ŋ", "Ƣ", "!", "¡"]
+FUNCTIONS = ["+", "-", "×", "÷", "ⁱ", "g", "¶", "Đ", "Ŋ", "Ƣ", "!", "¡", "□"]
 
 INDICATORS = ["↹", "{", "}"]
 
 codepage = FUNCTIONS + INDICATORS
+# codepage += "".join(chr(x) for x in range(32, 126+1))
+
+assert len(codepage) <= 256
