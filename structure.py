7f382fce74721279b38ffaffcfee1cc60525314a
Tams-Tams <Tams.Mathe@gmail.com>
2022-03-23 16:49:11 +0100

    Add bugs

--- structure.py
+++ structure.py
@@ -1,4 +1,5 @@
 from tokenizer import Token, TokenType, tokenize
+from collections import deque
 
 
 class ForLoop:
@@ -29,12 +30,27 @@ def structure_forLoop(tokens):
     loop_number = Token(TokenType.NUMBER, 10)
 
     structured_tokens = []
+    tokens_deque = deque(tokens)
 
-    for token in tokens:
+    while tokens_deque:
+        token = tokens_deque.popleft()
+
         if token.value == "↹":
             loop_started = 1  # stage 1, outside {}
         elif token.value == "{":
             loop_started = 2  # stage 2, inside {}
+            close_bracket = 0
+
+            tokens_copy = list(tokens_deque.copy())
+
+            for index in range(len(tokens_copy) - 1, 0, -1):
+                if tokens_copy[index].value == "}":
+                    close_bracket = index
+                    break
+                tokens_deque.popleft()
+
+            loop_content = structure_forLoop(tokens_copy[:close_bracket])
+
         elif token.value == "}":  # end of loop
             structured_tokens.append(ForLoop(loop_number, loop_content))
 
@@ -51,10 +67,10 @@ def structure_forLoop(tokens):
 
     return structured_tokens
 
+
 def structure(tokens):
     return structure_forLoop(tokens)
 
 
 if __name__ == "__main__":
-    print(structure(tokenize("+2 3↹4{¶1} 5")))
-    print(structure(tokenize("+2 3↹{¶1} 5")))
+    print(structure(tokenize("↹{1↹{2}}")))
